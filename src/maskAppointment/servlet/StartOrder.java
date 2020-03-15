9fd95df85b9d854340571c1367bae0fa90795095
StrickerLin <45957193+StrickerLin@users.noreply.github.com>
2020-03-15 21:41:35 +0800

    Update StartOrder.java

--- src/maskAppointment/servlet/StartOrder.java
+++ src/maskAppointment/servlet/StartOrder.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class StartOrder
+ * servelet implementation class StartOrder
  */
 @WebServlet("/StartOrder")
 public class StartOrder extends HttpServlet {

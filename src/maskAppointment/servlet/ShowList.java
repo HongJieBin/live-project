2c84ef46dcac2e3cc2cdad9056fe116f8d9d4121
Just-hy <525427312@qq.com>
2020-03-15 23:50:16 +0800

    update ShowList.java

--- src/maskAppointment/servlet/ShowList.java
+++ src/maskAppointment/servlet/ShowList.java
@@ -9,38 +9,62 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import maskAppointment.dao.BallotRecordDAO;
+import maskAppointment.dao.BallotRecordDAOImpl;
+import maskAppointment.dao.SystemTimeDAO;
+import maskAppointment.dao.SystemTimeDAOImpl;
+import maskAppointment.pojo.BallotRecord;
+
 /**
  * Servlet implementation class ShowList
  */
 @WebServlet("/ShowList")
 public class ShowList extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ShowList() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		ProofDAO proofDAO = new ProofDAO();
-		List<Proof> proofs =proofDAO.list();
-		request.setAttribute("proof", proofs);
-		//把数据设置并传入前端页面
-		request.getRequestDispatcher("ShowList.jsp").forward(request,response);
+	public ShowList() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 获取当前的最新轮次
+		int temp = 0; // 保存当前的轮次
+		SystemTimeDAO systemTimeDAO = new SystemTimeDAOImpl();
+		temp = systemTimeDAO.getTotal();// 这是当前的轮次
+		try {
+		BallotRecordDAO ballotRecordDAO = new BallotRecordDAOImpl();
+		List<BallotRecord> ballotRecords = ballotRecordDAO.getList();
+		for (BallotRecord i : ballotRecords) {
+			if (temp==i.getBallotRound()) {// 如果中签轮次是最新的，设置数据
+				request.setAttribute("ballotRecords", ballotRecords);
+			}
+		}}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+
+
+		// 设置的数据传入前端界面
+		request.getRequestDispatcher("ballot.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
